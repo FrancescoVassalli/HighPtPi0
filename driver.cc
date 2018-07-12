ce8e0cfbeec61c231189d0ed70652fed2ad88187
FrancescoVassalli <Francesco.Vassalli@colorado.edu>
2018-07-12 19:02:18 -0400

    preparing for condor

--- driver.cc
+++ driver.cc
@@ -1,8 +1,13 @@
 #include "PionPythiaGen.h"
 
+using namespace std;
 
-int main(){
-	PionPythiaGen generator("PionPythiaGenpT5");
-	generator.init("5");
-	generator.run(1,10);
+int main(int argc, char const *argv[]){
+  string fileOut = string(argv[1]);
+  string pTHat = string(argv[2]);
+  float gammaCut= strtod(argv[3],NULL);
+  long nEvents =strtol(argv[4],NULL,10);  // 5000000;
+	PionPythiaGen generator(fileOut);
+	generator.init(pTHat);
+	generator.run(nEvents,gammaCut);
 }
